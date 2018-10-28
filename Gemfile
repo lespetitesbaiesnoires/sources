8f62edec430587060c02501de6116b00d080fbce
Travis CI <sebastien@boyron.eu>
2018-10-28 17:50:36 +0100

    clean and setup password

--- Gemfile
+++
@@ -1,9 +0,0 @@
-source "https://rubygems.org"
-
-gem "jekyll"
-gem "html-proofer"
-
-group :jekyll_plugins do
-  gem "jekyll-paginate-v2"
-  gem "jekyll-feed"
-end
--- localbuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x 
-
-rm -rf _site
-bundle exec jekyll build
-bundle exec htmlproofer _site --disable-external
-bundle exec jekyll serve
